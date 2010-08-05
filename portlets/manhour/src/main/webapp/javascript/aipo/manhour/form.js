8d319c50b42d56d92bb393a74734d6cd083fb9ec
nbeppu <nbeppu@gmail.com>
2010-08-05 06:24:34 +0000

    ダイアログのエラーメッセージ表示箇所と、一覧表示のエラーメッセージ表示箇所が区別されるようにした。
    Update issue 45
    Status: PendingTesting
    
    ダイアログのエラーメッセージ表示箇所と、一覧表示のエラーメッセージ表示箇所に同一のIDが割り振られていたために発生した問題。
    それぞれ異なるIDに変更したことで、エラーメッセージが正しい場所に表示されるようにした。
    
    git-svn-id: http://aipo.googlecode.com/svn/trunk@47 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- portlets/manhour/src/main/webapp/javascript/aipo/manhour/form.js
+++ portlets/manhour/src/main/webapp/javascript/aipo/manhour/form.js
@@ -2,17 +2,17 @@
  * Aipo is a groupware program developed by Aimluck,Inc.
  * Copyright (C) 2004-2008 Aimluck,Inc.
  * http://aipostyle.com/
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -39,14 +39,27 @@ aipo.manhour.onReceiveMessage = function(msg){
     if (dojo.byId('messageDiv')) {
         dojo.byId('messageDiv').innerHTML = msg;
     }
-
 }
+
+aipo.manhour.onReceiveMessageDiag = function(msg){
+    if(!msg) {
+        var arrDialog = dijit.byId("modalDialog");
+        if(!!arrDialog){
+            arrDialog.hide();
+        }
+        aipo.portletReload('manhour');
+    }
+    if (dojo.byId('messageDivDiag')) {
+        dojo.byId('messageDivDiag').innerHTML = msg;
+    }
+}
+
 
 aipo.manhour.onChangeGroup = function(base_url, p_id){
     var select = dojo.byId('target_group_name');
     var group_name = select.options[select.selectedIndex].value;
     var exec_url = base_url+"&target_group_name="+group_name+"&target_user_id=";
-    
+
     aipo.viewPage(exec_url, p_id);
 }
 
@@ -64,7 +77,7 @@ aipo.manhour.onChangeCategory = function(base_url, p_id){
     var select_category = dojo.byId('commoncategory');
     var category_id = select_category.options[select_category.selectedIndex].value;
     var exec_url = base_url+"&category_id="+category_id;
-    
+
     aipo.viewPage(exec_url, p_id);
 }
 
@@ -74,6 +87,6 @@ aipo.manhour.onChangeDate = function(base_url, p_id){
     var year = select_year.options[select_year.selectedIndex].value;
     var month = select_month.options[select_month.selectedIndex].value;
     var exec_url = base_url+"&view_date_year="+year+"&view_date_month="+month;
-    
+
     aipo.viewPage(exec_url, p_id);
 }
