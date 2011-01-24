b4c8b224ba886d31f50d929ba2b3baa4790ff4ca
nbeppu <nbeppu@gmail.com>
2011-01-24 08:44:19 +0000

    Update issue 256
    /trunk へマージ
    ・gadgets
    
    git-svn-id: http://aipo.googlecode.com/svn/trunk@649 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

---
+++ portlets/gadgets/src/main/java/com/aimluck/eip/gadgets/GadgetsAdminSelectData.java
@@ -0,0 +1,137 @@
+/*
+ * Aipo is a groupware program developed by Aimluck,Inc.
+ * Copyright (C) 2004-2011 Aimluck,Inc.
+ * http://aipostyle.com/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aimluck.eip.gadgets;
+
+import java.util.jar.Attributes;
+
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+import com.aimluck.eip.common.ALAbstractSelectData;
+import com.aimluck.eip.common.ALApplication;
+import com.aimluck.eip.common.ALDBErrorException;
+import com.aimluck.eip.common.ALEipConstants;
+import com.aimluck.eip.common.ALPageNotFoundException;
+import com.aimluck.eip.modules.actions.common.ALAction;
+import com.aimluck.eip.orm.query.ResultList;
+import com.aimluck.eip.services.social.ALSocialApplicationFactoryService;
+import com.aimluck.eip.services.social.ALSocialApplicationHandler;
+import com.aimluck.eip.services.social.model.ALApplicationGetRequest;
+import com.aimluck.eip.services.social.model.ALApplicationGetRequest.Status;
+import com.aimluck.eip.util.ALEipUtils;
+
+/**
+ * 
+ */
+public class GadgetsAdminSelectData extends
+    ALAbstractSelectData<ALApplication, ALApplication> {
+
+  private ALSocialApplicationHandler appHandler = null;
+
+  @Override
+  public void init(ALAction action, RunData rundata, Context context)
+      throws ALPageNotFoundException, ALDBErrorException {
+    super.init(action, rundata, context);
+
+    appHandler =
+      ALSocialApplicationFactoryService
+        .getInstance()
+        .getSocialApplicationHandler();
+  }
+
+  /**
+   * @param rundata
+   * @param context
+   * @return
+   * @throws ALPageNotFoundException
+   * @throws ALDBErrorException
+   */
+  @Override
+  protected ResultList<ALApplication> selectList(RunData rundata,
+      Context context) throws ALPageNotFoundException, ALDBErrorException {
+    Status status = Status.ALL;
+    String filter = ALEipUtils.getTemp(rundata, context, LIST_FILTER_STR);
+    if ("1".equals(filter)) {
+      status = Status.ACTIVE;
+      current_filter = filter;
+    } else if ("0".equals(filter)) {
+      status = Status.INACTIVE;
+      current_filter = filter;
+    } else if ("all".equals(filter)) {
+      status = Status.ALL;
+      current_filter = filter;
+    }
+    return appHandler.getApplicationList(new ALApplicationGetRequest()
+      .withLimit(getRowsNum())
+      .withPage(current_page)
+      .withStatus(status));
+  }
+
+  /**
+   * @param rundata
+   * @param context
+   * @return
+   * @throws ALPageNotFoundException
+   * @throws ALDBErrorException
+   */
+  @Override
+  protected ALApplication selectDetail(RunData rundata, Context context)
+      throws ALPageNotFoundException, ALDBErrorException {
+
+    String appId =
+      ALEipUtils.getTemp(rundata, context, ALEipConstants.ENTITY_ID);
+
+    return appHandler.getApplication(new ALApplicationGetRequest().withAppId(
+      appId).withStatus(Status.ALL).withIsDetail(true));
+  }
+
+  /**
+   * @param model
+   * @return
+   * @throws ALPageNotFoundException
+   * @throws ALDBErrorException
+   */
+  @Override
+  protected Object getResultData(ALApplication model)
+      throws ALPageNotFoundException, ALDBErrorException {
+    return model;
+  }
+
+  /**
+   * @param model
+   * @return
+   * @throws ALPageNotFoundException
+   * @throws ALDBErrorException
+   */
+  @Override
+  protected Object getResultDataDetail(ALApplication model)
+      throws ALPageNotFoundException, ALDBErrorException {
+    return model;
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  protected Attributes getColumnMap() {
+    return null;
+  }
+
+}
---
+++ portlets/gadgets/src/main/java/com/aimluck/eip/modules/actions/gadgets/GadgetsAction.java
@@ -0,0 +1,110 @@
+/*
+ * Aipo is a groupware program developed by Aimluck,Inc.
+ * Copyright (C) 2004-2010 Aimluck,Inc.
+ * http://aipostyle.com/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aimluck.eip.modules.actions.gadgets;
+
+import org.apache.jetspeed.portal.portlets.VelocityPortlet;
+import org.apache.jetspeed.services.logging.JetspeedLogFactoryService;
+import org.apache.jetspeed.services.logging.JetspeedLogger;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+import com.aimluck.eip.common.ALEipUser;
+import com.aimluck.eip.gadgets.GadgetContext;
+import com.aimluck.eip.modules.actions.common.ALBaseAction;
+import com.aimluck.eip.orm.Database;
+import com.aimluck.eip.services.social.ALSocialApplicationFactoryService;
+import com.aimluck.eip.services.social.ALSocialApplicationHandler;
+import com.aimluck.eip.util.ALEipUtils;
+
+/**
+ *
+ *
+ */
+public class GadgetsAction extends ALBaseAction {
+
+  /** logger */
+  @SuppressWarnings("unused")
+  private static final JetspeedLogger logger = JetspeedLogFactoryService
+    .getLogger(GadgetsAction.class.getName());
+
+  /**
+   * 
+   * @param portlet
+   * @param context
+   * @param rundata
+   * @throws java.lang.Exception
+   */
+  @Override
+  protected void buildNormalContext(VelocityPortlet portlet, Context context,
+      RunData rundata) throws Exception {
+    buildCommonContext(portlet, context, rundata, false);
+  }
+
+  /**
+   * 
+   * @param portlet
+   * @param context
+   * @param rundata
+   */
+  @Override
+  protected void buildMaximizedContext(VelocityPortlet portlet,
+      Context context, RunData rundata) {
+    buildCommonContext(portlet, context, rundata, true);
+  }
+
+  protected void buildCommonContext(VelocityPortlet portlet, Context context,
+      RunData rundata, boolean isMaximized) {
+
+    String appId = portlet.getPortletConfig().getInitParameter("aid");
+    String url = portlet.getPortletConfig().getInitParameter("url");
+
+    boolean isActive = true;
+    if (url == null || url == "") {
+      url = portlet.getPortletConfig().getInitParameter("p1a-url");
+      appId = url;
+    } else {
+      isActive = checkApplicationAvailability(appId);
+    }
+
+    ALEipUser user = ALEipUtils.getALEipUser(rundata);
+    String orgId = Database.getDomainName();
+    String viewer =
+      new StringBuilder(orgId)
+        .append(":")
+        .append(user.getName().getValue())
+        .toString();
+
+    GadgetContext gadgetContext = new GadgetContext(rundata, viewer, url);
+
+    context.put("gadgetContext", gadgetContext);
+    context.put("isActive", isActive);
+
+    setTemplate(rundata, isMaximized ? "gadgets-list" : "gadgets");
+  }
+
+  protected boolean checkApplicationAvailability(String appId) {
+    ALSocialApplicationHandler appHandler =
+      ALSocialApplicationFactoryService
+        .getInstance()
+        .getSocialApplicationHandler();
+
+    return appHandler.checkApplicationAvailability(appId);
+  }
+}
