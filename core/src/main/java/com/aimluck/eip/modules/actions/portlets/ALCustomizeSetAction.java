bde6b8107fe87b57b614a60e8386f2cc644bbd09
nbeppu <nbeppu@gmail.com>
2011-02-15 02:02:30 +0000

    Update issue 292 Status: PendingReview
    管理者権限を付与すると、ポートレットカスタマイズで管理者用のポートレットが選べてしまう問題を修正しました。
    管理者用のポートレットは、どのような場合であってもポートレットカスタマイズ画面には表示されないようにしました。
    
    git-svn-id: http://aipo.googlecode.com/svn/trunk@743 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- core/src/main/java/com/aimluck/eip/modules/actions/portlets/ALCustomizeSetAction.java
+++ core/src/main/java/com/aimluck/eip/modules/actions/portlets/ALCustomizeSetAction.java
@@ -103,11 +103,11 @@ import com.aimluck.eip.util.ALEipUtils;
 
 /**
  * This action implements the default portletset behavior customizer
- * 
+ *
  * <p>
  * Don't call it from the URL, the Portlet and the Action are automatically
  * associated through the registry PortletName
- * 
+ *
  * @author <a href="mailto:raphael@apache.org">Rapha�l Luta</a>
  */
 public class ALCustomizeSetAction extends VelocityPortletAction {
@@ -790,7 +790,7 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
 
   /**
    * Get the security reference from the outer portlet set
-   * 
+   *
    * @param path
    *          the psml locator path
    * @return the security reference of the referenced resource
@@ -808,7 +808,7 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
            * There is no way to do a check on a Portlets element, only a Entry
            * element. This can easily be added, but Im just under a release
            * right now and it could be perceived as too destabilizing -- david
-           * 
+           *
            * if (JetspeedSecurity.checkPermission((JetspeedUser)
            * rundata.getUser(), rootSet, JetspeedSecurity.PERMISSION_VIEW)) {
            */
@@ -906,14 +906,14 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
    * Set the skin in the PSML and the current PortletConfig using the HTML
    * parameter "skin". If the parmeter is missing or 'blank', then the skin is
    * set to null.
-   * 
+   *
    */
 
   /**
    * Set the skin in the PSML and the current PortletConfig using the HTML
    * parameter "skin". If the parmeter is missing or 'blank', then the skin is
    * set to null.
-   * 
+   *
    */
   @SuppressWarnings("deprecation")
   public void doSkin(RunData rundata, Context context) {
@@ -992,7 +992,7 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
    * Set the SecuirtyRef in the PSML and the current PortletConfig using the
    * HTML parameter "securityRef". If the parmeter is missing or 'blank', then
    * the SecuriyReference is set to null.
-   * 
+   *
    */
   public void doSecurity(RunData rundata, Context context) {
     // we should first retrieve the portlet to customize and its parameters
@@ -1038,7 +1038,8 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
         JetspeedSecurity.PERMISSION_VIEW)
         && ((!entry.isHidden())
           && (!entry.getType().equals(PortletEntry.TYPE_ABSTRACT)) && entry
-          .hasMediaType(mediaType))) {
+          .hasMediaType(mediaType))
+        && !entry.getSecurityRef().getParent().equals("admin-view")) {
         list.add(entry);
       }
     }
@@ -1188,7 +1189,7 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
 
   /**
    * Builds a list of all portlet categories
-   * 
+   *
    * @param RunData
    *          current requests RunData object
    * @param List
@@ -1612,7 +1613,7 @@ public class ALCustomizeSetAction extends VelocityPortletAction {
    * Add an element to the "table" or "work" objects. If the element is
    * unconstrained, and the position is within the number of columns, then the
    * element is added to "table". Othewise the element is added to "work"
-   * 
+   *
    * @param element
    *          to add
    * @param table
