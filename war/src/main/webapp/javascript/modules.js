0e727ca99133c797169bd583fd5f2d5bf04da1c0
nbeppu <nbeppu@gmail.com>
2011-02-22 09:28:40 +0000

    Update issue 348
    クロスドメインの外部リソースをサポートしました。
    ハイブリッド構成になってまして、
    外部リソースがONの場合は、クロスドメイン用の dojo が読み込まれるようになります。
    
    
    git-svn-id: http://aipo.googlecode.com/svn/trunk@770 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- war/src/main/webapp/javascript/modules.js
+++ war/src/main/webapp/javascript/modules.js
@@ -17,8 +17,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+if(typeof djConfig["xDomainBasePath"] == "undefined") {
 dojo.registerModulePath("dojo","../dojo");
 dojo.registerModulePath("dijit","../dijit");
 dojo.registerModulePath("dojox","../dojox");
 dojo.registerModulePath("aimluck","../aimluck");
 dojo.registerModulePath("aipo","../aipo");
+} else {
+djConfig.useXDomain = true;
+dojo.registerModulePath("dojo", djConfig.xDomainBasePath + "/dojo");
+dojo.registerModulePath("dijit", djConfig.xDomainBasePath + "/dijit");
+dojo.registerModulePath("dojox", djConfig.xDomainBasePath + "/dojox");
+dojo.registerModulePath("aimluck", djConfig.xDomainBasePath + "/aimluck");
+dojo.registerModulePath("aipo", djConfig.xDomainBasePath + "/aipo");
+}
