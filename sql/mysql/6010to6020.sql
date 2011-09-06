65cde33bfd3d37d928b00d998759b99f9f8eae21
altakase <yusuke.takase.al@gmail.com>
2011-09-06 01:08:38 +0000

    git-svn-id: http://aipo.googlecode.com/svn/trunk@1478 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- sql/mysql/6010to6020.sql
+++ sql/mysql/6010to6020.sql
@@ -1,8 +1,9 @@
---- modify table struct
-ALTER TABLE eip_t_schedule ADD COLUMN mail_flag CHAR(1) AFTER edit_flag;
-ALTER TABLE eip_t_cabinet_file ADD COLUMN counter INTEGER(11) AFTER note;
+-- 20110905
+-- modify table struct
+ALTER TABLE `eip_t_schedule` ADD `mail_flag` CHAR(1) ;
+ALTER TABLE `eip_t_cabinet_file` ADD `counter` INTEGER ;
 
---- update data
-UPDATE eip_t_schedule SET mail_flag = 'A' ;
-UPDATE eip_m_addressbook_company SET company_name = '', company_name_kana = '' WHERE company_id = 1;
-
+-- update data
+UPDATE `eip_t_schedule` SET `mail_flag` = 'A' ;
+UPDATE `eip_m_addressbook_company` SET `company_name` = '', `company_name_kana` = '' WHERE `company_id` = 1;
+-- 20110905
