f90e29f6092144695d3657e36657fe2b41639458
alkoshikawa <ges.jonieet@gmail.com>
2011-10-21 04:55:59 +0000

    sql実行時にエラーがでていたのを修正
    git-svn-id: http://aipo.googlecode.com/svn/trunk@1845 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- sql/mysql/6020to6030.sql
+++ sql/mysql/6020to6030.sql
@@ -15,7 +15,7 @@ UPDATE eip_t_todo_category SET update_user_id = user_id;
 
 -- add new ACL setting
 INSERT INTO eip_t_acl_portlet_feature VALUES(null,'todo_category_other','ToDo（他ユーザのカテゴリ）操作',27);
-INSERT INTO eip_t_acl_role VALUES(null, 'ToDo（他ユーザのカテゴリ）管理者', (SELECT feature_id from eip_t_acl_portlet_feature WHERE feature_name = 'todo_category_other' limit 1),27,'＊詳細表示、編集、削除は一覧表示の権限を持っていないと使用できません');
+INSERT INTO eip_t_acl_role VALUES(null, 'ToDo（他ユーザのカテゴリ）管理者', (SELECT feature_id from eip_t_acl_portlet_feature WHERE feature_name = 'todo_category_other' limit 1),27,'＊詳細表示、編集、削除は一覧表示の権限を持っていないと使用できません', null, null);
 -- 20111019
 
 -- 20111021
