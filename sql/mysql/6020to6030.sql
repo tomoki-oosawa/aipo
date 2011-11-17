e5c33ad5c4a1757b625372efdd3ae5567436b0cc
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2011-11-17 02:52:32 +0000

    Update Issue 631 更新用IDの指定方法変更
    git-svn-id: http://aipo.googlecode.com/svn/trunk@2026 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- sql/mysql/6020to6030.sql
+++ sql/mysql/6020to6030.sql
@@ -41,8 +41,8 @@ UPDATE `eip_t_cabinet_file` SET `counter` = 0 WHERE `counter` IS NULL;
 -- 20111028
 
 -- 20111116
-UPDATE `eip_t_acl_portlet_feature` SET `feature_alias_name` = 'スケジュール（設備の予約）操作' WHERE `feature_id` = 113;
-UPDATE `eip_t_acl_role` SET `role_name` = 'スケジュール（設備の予約）操作' WHERE `role_id` = 3;
+UPDATE `eip_t_acl_portlet_feature` SET `feature_alias_name` = 'スケジュール（設備の予約）操作' WHERE `feature_name` = 'schedule_facility' AND `feature_alias_name` = 'スケジュール（施設の予約）操作';
+UPDATE `eip_t_acl_role` SET `role_name` = 'スケジュール（設備の予約）操作' WHERE feature_id IN (SELECT feature_id FROM eip_t_acl_portlet_feature WHERE feature_name = 'schedule_facility') AND `role_name` = 'スケジュール（施設の予約）管理者';
 -- 20111116
 
 -- 20111116
