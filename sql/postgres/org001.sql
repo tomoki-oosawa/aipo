e906bca905d73844e5695fc6e97d5cae1fc4426a
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2011-12-15 07:50:17 +0000

    Update issue 650 SQL調整
    git-svn-id: http://aipo.googlecode.com/svn/trunk@2214 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- sql/postgres/org001.sql
+++ sql/postgres/org001.sql
@@ -1623,7 +1623,7 @@ INSERT INTO EIP_T_ACL_ROLE VALUES(20,'アドレス帳（社外グループ）管
 
 -- timecard
 INSERT INTO EIP_T_ACL_ROLE VALUES(21,'タイムカード（自分のタイムカード）管理者',171,47,'＊追加、編集、外部出力は一覧表示の権限を持っていないと使用できません');
-INSERT INTO EIP_T_ACL_ROLE VALUES(22,'タイムカード（他人のタイムカード）管理者',172,33,'＊自分のタイムカード一覧表示の権限を持っていないと使用できません\n＊外部出力は一覧表示の権限を持っていないと使用できません');
+INSERT INTO EIP_T_ACL_ROLE VALUES(22,'タイムカード（他人のタイムカード）管理者',172,33,'＊自分のタイムカード一覧表示の権限を持っていないと使用できません ＊外部出力は一覧表示の権限を持っていないと使用できません');
 
 -- cabinet
 INSERT INTO EIP_T_ACL_ROLE VALUES(23,'共有フォルダ（ファイル）管理者',181,31,'＊詳細表示、追加、編集、削除は一覧表示の権限を持っていないと使用できません');
@@ -1639,11 +1639,10 @@ INSERT INTO EIP_T_ACL_ROLE VALUES(28,'プロジェクト管理（他ユーザー
 INSERT INTO EIP_T_ACL_ROLE VALUES(29,'ポートレット管理者',201,29,NULL);
 
 --report
-INSERT INTO EIP_T_ACL_ROLE VALUES(30, '報告書（自分の報告書）管理者',211,31,'＊追加、編集、削除は一覧表示と詳細表示の権限を持っていないと使用できません');
-INSERT INTO EIP_T_ACL_ROLE VALUES(31,'報告書（他ユーザーの報告書）管理者',212,3,'＊詳細表示は一覧表示の権限を持っていないと使用できません');
--
+INSERT INTO EIP_T_ACL_ROLE VALUES(31, '報告書（自分の報告書）管理者',211,31,'＊追加、編集、削除は一覧表示と詳細表示の権限を持っていないと使用できません');
+INSERT INTO EIP_T_ACL_ROLE VALUES(32,'報告書（他ユーザーの報告書）管理者',212,3,'＊詳細表示は一覧表示の権限を持っていないと使用できません');
 
-SELECT setval('pk_eip_t_acl_role',32);
+SELECT setval('pk_eip_t_acl_role',33);
 
 INSERT INTO EIP_T_BLOG_THEMA VALUES(1,'未分類','',0,0,NULL ,NULL);
 SELECT setval('pk_eip_t_blog_thema',1);
