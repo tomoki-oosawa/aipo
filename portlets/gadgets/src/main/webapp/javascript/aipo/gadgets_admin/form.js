f6e6400180f0d9272e9c0ffea6335000da5a711e
alkoshikawa <ges.jonieet@gmail.com>
2011-12-21 08:17:52 +0000

    Update issue 656
    git-svn-id: http://aipo.googlecode.com/svn/trunk@2278 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- portlets/gadgets/src/main/webapp/javascript/aipo/gadgets_admin/form.js
+++ portlets/gadgets/src/main/webapp/javascript/aipo/gadgets_admin/form.js
@@ -67,3 +67,44 @@ aipo.gadgets_admin.ajaxDeleteSubmit = function(button, url, indicator_id, portle
 	    aimluck.io.submit(button.form, indicator_id, portlet_id, receive);
 	  }
 };
+
+/*
+ * Aipo is a groupware program developed by Aimluck,Inc.
+ * Copyright (C) 2004-2011 Aimluck,Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var Mode;
+var form;
+aipo.gadgets_admin.beforeSubmit = function(button,portlet_id, mode) {
+    dojo.byId(portlet_id + "-mode").value = mode;
+    form=button.form;
+    Mode=mode;
+}
+
+
+aipo.gadgets_admin.onReceiveMessageUpdate = function(msg){
+	dojo.byId('caution_update').innerHTML
+	=dojo.byId('caution_default').innerHTML
+	=dojo.byId('caution_all_user').innerHTML
+	="";
+	if (dojo.byId('caution_'+Mode)) {
+		dojo.byId('caution_'+Mode).innerHTML =!msg?"更新が完了しました。":"設定に失敗しました。時間をおいてから再度試してください。";
+		aimluck.io.disableForm(form,false);
+    }
+}
+
+
