9c1abe5cbe039d250f87d74e34d18aaca3f0dd5a
alkoshikawa <ges.jonieet@gmail.com>
2012-02-28 01:14:31 +0000

    Update issue 705 時間表記を「H:mm」にした
    git-svn-id: http://aipo.googlecode.com/svn/trunk@2657 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- commons/src/main/java/com/aimluck/commons/field/ALDateTimeField.java
+++ commons/src/main/java/com/aimluck/commons/field/ALDateTimeField.java
@@ -45,7 +45,7 @@ public class ALDateTimeField extends ALAbstractField {
   public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy/MM/dd HH:mm";
 
   /** 時刻の表示フォーマット */
-  public static final String DEFAULT_TIME_FORMAT = "H:m";
+  public static final String DEFAULT_TIME_FORMAT = "H:mm";
 
   /** 日付の表示フォーマット */
   public static final String DEFAULT_DATE_FORMAT = "yyyy/MM/dd";
