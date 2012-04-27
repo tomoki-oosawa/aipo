16bf8c85e740ea3038b264b5f250e6e25ffa3ba4
nbeppu <nbeppu@gmail.com>
2012-04-27 09:09:49 +0000

    update issue 753 スケジュールのdataPickerダイアログの修正(refs #17860)
    git-svn-id: http://aipo.googlecode.com/svn/trunk@3090 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- war/src/main/webapp/javascript/aipo/widget/DropdownDatepicker.xd.js
+++ war/src/main/webapp/javascript/aipo/widget/DropdownDatepicker.xd.js
@@ -46,6 +46,27 @@ dojo.declare("aipo.widget.DropdownDatepicker", [aimluck.widget.Dropdown], {
     callback: function(){},
     listWidgetId:"datewidget",
     templateString:"<div class=\"dijit dijitLeft dijitInline\"><div dojoAttachEvent=\"onmouseenter:_onMouse,onmouseleave:_onMouse,onmousedown:_onMouse,onclick:_onDropDownClick,onkeydown:_onDropDownKeydown,onblur:_onDropDownBlur,onkeypress:_onKey\"\n\t style=\"float:left;\"><div class='dijitRight'>\n\t<span class=\"\" type=\"${type}\"\n\t\tdojoAttachPoint=\"focusNode,titleNode\" waiRole=\"button\" waiState=\"haspopup-true,labelledby-${id}_label\"\n\t\t><span class=\"\" \tdojoAttachPoint=\"containerNode,popupStateNode\"\n\t\tid=\"${id}_label\"><img src=\"${iconURL}\" alt=\"${iconAlt}\" style=\"cursor:pointer;cursor:hand;padding-right:2px\" align=\"top\" />\n\t</span></div></div><div class=\"alignleft\"><span name=\"${dateId}_view\" id=\"${dateId}_view\" dojoAttachPoint=\"inputNode\" style=\"vertical-align:middle;background:#ffffff ;border:0px;\" autocomplete=\"off\" readonly=\"readonly\"></span><span style=\"display:${displayCheck}\"><input name=\"${dateId}_check\" type=\"checkbox\" value=\"TRUE\" id=\"${dateId}_flag\" dojoAttachEvent=\"onclick:onCheckBlank\" /><label for=\"${dateId}_flag\">\u0020\u6307\u5b9a\u3057\u306a\u3044</label></span><input type=\"hidden\" id=\"${dateId}\" name=\"${dateId}\" value=\"${dateValue}\" dojoAttachPoint=\"valueNode\" /><input type=\"hidden\" id=\"${dateId}_year\" name=\"${dateId}_year\" value=\"\" dojoAttachPoint=\"valueYearNode\" /><input type=\"hidden\" id=\"${dateId}_month\" name=\"${dateId}_month\" value=\"\" dojoAttachPoint=\"valueMonthNode\" /><input type=\"hidden\" id=\"${dateId}_day\" name=\"${dateId}_day\" value=\"\" dojoAttachPoint=\"valueDayNode\" /></div></div>\n",
+
+    _openDropDown:function(){
+    	//android時に他のフォームを無効にする。
+
+    	aimluck.widget.Dropdown.prototype._openDropDown.apply(this);
+    	if(aipo.userAgent.isAndroid()){
+        	//input,select,buttonをdisable化
+        	dojo.query("input,select,button").forEach(function(val,index){
+        		val.disabled=true;
+        	});
+    	}
+    },
+    _closeDropDown:function(){
+    	aimluck.widget.Dropdown.prototype._closeDropDown.apply(this);
+    	if(aipo.userAgent.isAndroid()){
+        	//input,select,buttonをdisable化
+        	dojo.query("input,select,button:not(.disabled)").forEach(function(val,index){
+        		val.disabled=false;
+        	});
+    	}
+    },
     postCreate: function(){
         this.inherited(arguments);
         var params = {
