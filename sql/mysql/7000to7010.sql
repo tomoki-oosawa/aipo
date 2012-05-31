f2f7ea5296ef076b66abaea4102119b86c4efa07
altakase <yusuke.takase.al@gmail.com>
2012-05-31 02:49:20 +0000

    UPDATE文が間違っている気がするので修正
    git-svn-id: http://aipo.googlecode.com/svn/trunk@3316 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- sql/mysql/7000to7010.sql
+++ sql/mysql/7000to7010.sql
@@ -1,4 +1,4 @@
 -- 20120530
 ALTER TABLE `turbine_user` ADD COLUMN `tutorial_forbid` VARCHAR (1) DEFAULT 'F';
-UPDATE TABLE `turbine_user` SET `tutorial_forbid` = 'T' ;
+UPDATE `turbine_user` SET `tutorial_forbid` = 'T' ;
 -- 20120530
