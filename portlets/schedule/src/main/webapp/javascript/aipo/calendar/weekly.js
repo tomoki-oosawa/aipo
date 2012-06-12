d4b8cc0530be2a0e052f13856a582c83c501cb41
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2012-06-12 06:45:36 +0000

    Update Issue 784 スケジュールを２つ並べた時に読み込み中のままになる
    git-svn-id: http://aipo.googlecode.com/svn/trunk@3408 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- portlets/schedule/src/main/webapp/javascript/aipo/calendar/weekly.js
+++ portlets/schedule/src/main/webapp/javascript/aipo/calendar/weekly.js
@@ -198,7 +198,7 @@ aipo.calendar.populateWeeklySchedule = function(_portletId, params) {
     if(dojo.byId('groupselect-' + _portletId).value =="pickup"){
     	_params += "&pickup=true";
     }
-    
+
     dojo.xhrGet({
         portletId: _portletId,
         url: ptConfig[_portletId].jsonUrl + _params,
@@ -1784,4 +1784,77 @@ dojo.declare("aipo.calendar.WeeklyTermScheduleAddDraggable", [aimluck.dnd.Dragga
         this.index = params.idx;
     }
 });
+
+
+
+aipo.schedule.initCalender=function(_portletId){
+	for(var i =0; i < ptConfig[_portletId].scheduleDivDaySum; i++) {
+      tmpDraggable = new aipo.calendar.WeeklyScheduleAddDraggable('scheduleDivAdd0'+i+'_'+_portletId , {idx: i});
+      tmpDraggable.portletId = _portletId;
+      tmpDraggable.index = i;
+    };
+
+    tmpDraggable = new aipo.calendar.WeeklyTermScheduleAddDraggable('termScheduleDivAdd_'+_portletId,  {idx: 0});
+    tmpDraggable.portletId = _portletId;
+
+    aipo.calendar.populateWeeklySchedule(_portletId);
+};
+
+aipo.schedule.groupSelectOnchange=function(obj, e, _portletId, mp){
+	var ajaxMemberSelectLoad=function(data, event) {
+		//paramsの編集+namesの変更
+		var html="";
+		mp.dropDown.removeMember(dojo.byId("member_to-"+_portletId));
+		for(var i=0;i<data.length;i++){
+			if (i != 0) {
+				html += ' ';
+			}
+			params+="&m_id="+data[i].name;
+			html+="<span class=\"dispUser color" + i +"\">" +data[i].aliasName+ "</span>";
+			aimluck.io.addOption(dojo.byId("member_to-"+_portletId), data[i].name, data[i].aliasName, true);
+		}
+		dojo.byId("member_to_input-"+_portletId).innerHTML=html;
+	};
+	var addtxt=dojo.query(".addUser",dojo.byId("memberpicker-"+_portletId));
+	switch(obj.value){
+		case "":
+			obj.selectedIndex=dojo.query('[value=pickup]',this)[0].index;//pickupへ流す。
+		case "pickup":
+			addtxt.removeClass("hide");
+			mp.dropDown.removeMember(dojo.byId("member_to-"+_portletId));
+			mp.dropDown.removeMember(dojo.byId("tmp_member_to-"+_portletId));
+			var p_mo = dojo.byId("picked_memberlist-"+_portletId).options;
+			for(var i = 0; i < p_mo.length; i++)(function(opt, index){
+			  opt.selected = true;
+			})(p_mo[i], i);
+			mp.dropDown.addMember(dojo.byId("picked_memberlist-"+_portletId), dojo.byId("tmp_member_to-"+_portletId));
+			mp.dropDown.addMember(dojo.byId("picked_memberlist-"+_portletId), dojo.byId("member_to-"+_portletId));
+			mp.inputMemberSync();
+
+			aipo.calendar.populateWeeklySchedule(_portletId);//スケジュール更新
+
+			//デフォルトを保存するためにajax送信
+			dojo.xhrGet({
+                portletId:_portletId,
+                url:dojo.byId("groupselect-defaulturl-"+_portletId).value,
+                encoding: "utf-8",
+                handleAs: "json-comment-filtered"
+			});
+			break;
+		default:
+			addtxt.addClass("hide");
+			var params="";
+			  dojo.xhrGet({
+                portletId:_portletId,
+                url:obj.value,
+                encoding: "utf-8",
+                handleAs: "json-comment-filtered",
+                load:ajaxMemberSelectLoad,
+				handle:function(){
+					aipo.calendar.populateWeeklySchedule(_portletId,params);//スケジュール更新
+				}
+			});
+			break;
+	}
+}
 }
