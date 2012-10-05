729a0f928e3bf74c7dcc1f80420be5206da26763
nbeppu <nbeppu@gmail.com>
2012-10-05 06:38:22 +0000

    開発 #22072 (FindBug)例外がスローされないのに例外をキャッチしています。com.aimluck.eip.modules.screens.CustomizeFormScreen.doOutput(RunData, Context)
    git-svn-id: http://aipo.googlecode.com/svn/trunk@4283 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- core/src/main/java/com/aimluck/eip/modules/screens/CustomizeFormScreen.java
+++ core/src/main/java/com/aimluck/eip/modules/screens/CustomizeFormScreen.java
@@ -38,7 +38,6 @@ import org.apache.velocity.context.Context;
 
 import com.aimluck.commons.field.ALStringField;
 import com.aimluck.eip.common.ALEipConstants;
-import com.aimluck.eip.common.ALPermissionException;
 import com.aimluck.eip.services.accessctl.ALAccessControlConstants;
 import com.aimluck.eip.util.ALCommonUtils;
 import com.aimluck.eip.util.ALEipUtils;
@@ -76,112 +75,109 @@ public class CustomizeFormScreen extends ALVelocityScreen {
    * @throws Exception
    */
   @Override
-  protected void doOutput(RunData rundata, Context context) throws Exception {
+  protected void doOutput(RunData rundata, Context context) {
     VelocityPortlet portlet = ALEipUtils.getPortlet(rundata, context);
-    try {
-      if (!ALEipUtils.getHasAuthority(
-        rundata,
-        context,
-        ALAccessControlConstants.VALUE_ACL_UPDATE)
-        && !ALEipUtils.getHasAuthority(
-          rundata,
-          context,
-          ALAccessControlConstants.VALUE_ACL_INSERT)) {
-        throw new ALPermissionException();
-      }
-      if (ALEipUtils.getHasAuthority(
+    if (!ALEipUtils.getHasAuthority(
+      rundata,
+      context,
+      ALAccessControlConstants.VALUE_ACL_UPDATE)
+      && !ALEipUtils.getHasAuthority(
         rundata,
         context,
         ALAccessControlConstants.VALUE_ACL_INSERT)) {
-        context.put("insert", 1);
-      } else {
-        context.put("insert", 0);
-      }
-      JetspeedRunData jdata = (JetspeedRunData) rundata;
-      Profile profile = jdata.getProfile();
-      String mediaType = profile.getMediaType();
-
-      // make the list of already used panes/portlets available through the
-      // 'runs'
-      // reference
-      List<Entry> portletList = new ArrayList<Entry>();
-
-      String pid = rundata.getParameters().get("js_pane");
-      Portlets tabPortlets = profile.getDocument().getPortletsById(pid);
-      Entry[] currentPortletEntries = tabPortlets.getEntriesArray();
-      for (Entry entry : currentPortletEntries) {
-        portletList.add(entry);
-      }
-      context.put("runs", portletList);
-
-      Portlets[] portletArrays =
-        (((JetspeedRunData) rundata).getProfile().getDocument().getPortlets())
-          .getPortletsArray();
-      ALStringField pageTitle = new ALStringField();
-      String mypageId = "";
-      for (Portlets p : portletArrays) {
-        if ("マイページ".equals(p.getTitle())) {
-          mypageId = p.getId();
-        }
-        if (p.getId().equals(pid)) {
-          MetaInfo info = p.getMetaInfo();
-          pageTitle.setValue(info.getTitle());
-        }
-      }
-
-      context.put("isMypage", "マイページ".equals(pageTitle.getValue()));
-      context.put("mypageId", mypageId);
-      context.put("pageTitle", pageTitle);
-
-      int start = rundata.getParameters().getInt("start", -1);
-      if (start < 0) {
-        start = 0;
-        PortletSessionState.clearAttribute(rundata, USER_SELECTIONS);
-        PortletSessionState.clearAttribute(rundata, PORTLET_LIST);
-      }
-
-      List<PortletEntry> allPortlets = new ArrayList<PortletEntry>();
-      List<PortletEntry> portlets =
-        CustomizeUtils.buildPortletList(rundata, mediaType, allPortlets);
-      Map<String, PortletEntry> userSelections =
-        CustomizeUtils.getUserSelections(rundata);
-      // List<BaseCategory> categories =
-      // CustomizeUtils.buildCategoryList(rundata, mediaType, allPortlets);
-      // context.put("categories", categories);
-      // context.put("parents", PortletFilter.buildParentList(allPortlets));
-      // addFiltersToContext(rundata, context);
-
-      int size = getSize(portlet);
-      int end = Math.min(start + size, portlets.size());
-      if (start > 0) {
-        context.put("prev", String.valueOf(Math.max(start - size, 0)));
-      }
-      if (start + size < portlets.size()) {
-        context.put("next", String.valueOf(start + size));
-      }
-      context.put("browser", portlets.subList(start, end));
-      context.put("size", Integer.valueOf(size));
-      context.put(UI_PORTLETS_SELECTED, userSelections);
-
-      context.put("portlets", portlets);
-      context.put("js_peid", pid);
-      rundata.getParameters().setString("js_peid", pid);
-
-      context.put("controller", tabPortlets.getController().getName());
-      context.put(ALEipConstants.SECURE_ID, rundata.getUser().getTemp(
-        ALEipConstants.SECURE_ID));
-
-      ALEipUtils.setupContext(rundata, context);
-      // putData(rundata, context);
-
-      context.put("l10n", ALLocalizationUtils.createLocalization(rundata));
-      context.put("utils", new ALCommonUtils());
-
-      String layout_template = "portlets/html/ja/ajax-customize-form.vm";
-      setTemplate(rundata, context, layout_template);
-    } catch (Exception ex) {
       ALEipUtils.redirectPermissionError(rundata);
+      return;
     }
+    if (ALEipUtils.getHasAuthority(
+      rundata,
+      context,
+      ALAccessControlConstants.VALUE_ACL_INSERT)) {
+      context.put("insert", 1);
+    } else {
+      context.put("insert", 0);
+    }
+    JetspeedRunData jdata = (JetspeedRunData) rundata;
+    Profile profile = jdata.getProfile();
+    String mediaType = profile.getMediaType();
+
+    // make the list of already used panes/portlets available through the
+    // 'runs'
+    // reference
+    List<Entry> portletList = new ArrayList<Entry>();
+
+    String pid = rundata.getParameters().get("js_pane");
+    Portlets tabPortlets = profile.getDocument().getPortletsById(pid);
+    Entry[] currentPortletEntries = tabPortlets.getEntriesArray();
+    for (Entry entry : currentPortletEntries) {
+      portletList.add(entry);
+    }
+    context.put("runs", portletList);
+
+    Portlets[] portletArrays =
+      (((JetspeedRunData) rundata).getProfile().getDocument().getPortlets())
+        .getPortletsArray();
+    ALStringField pageTitle = new ALStringField();
+    String mypageId = "";
+    for (Portlets p : portletArrays) {
+      if ("マイページ".equals(p.getTitle())) {
+        mypageId = p.getId();
+      }
+      if (p.getId().equals(pid)) {
+        MetaInfo info = p.getMetaInfo();
+        pageTitle.setValue(info.getTitle());
+      }
+    }
+
+    context.put("isMypage", "マイページ".equals(pageTitle.getValue()));
+    context.put("mypageId", mypageId);
+    context.put("pageTitle", pageTitle);
+
+    int start = rundata.getParameters().getInt("start", -1);
+    if (start < 0) {
+      start = 0;
+      PortletSessionState.clearAttribute(rundata, USER_SELECTIONS);
+      PortletSessionState.clearAttribute(rundata, PORTLET_LIST);
+    }
+
+    List<PortletEntry> allPortlets = new ArrayList<PortletEntry>();
+    List<PortletEntry> portlets =
+      CustomizeUtils.buildPortletList(rundata, mediaType, allPortlets);
+    Map<String, PortletEntry> userSelections =
+      CustomizeUtils.getUserSelections(rundata);
+    // List<BaseCategory> categories =
+    // CustomizeUtils.buildCategoryList(rundata, mediaType, allPortlets);
+    // context.put("categories", categories);
+    // context.put("parents", PortletFilter.buildParentList(allPortlets));
+    // addFiltersToContext(rundata, context);
+
+    int size = getSize(portlet);
+    int end = Math.min(start + size, portlets.size());
+    if (start > 0) {
+      context.put("prev", String.valueOf(Math.max(start - size, 0)));
+    }
+    if (start + size < portlets.size()) {
+      context.put("next", String.valueOf(start + size));
+    }
+    context.put("browser", portlets.subList(start, end));
+    context.put("size", Integer.valueOf(size));
+    context.put(UI_PORTLETS_SELECTED, userSelections);
+
+    context.put("portlets", portlets);
+    context.put("js_peid", pid);
+    rundata.getParameters().setString("js_peid", pid);
+
+    context.put("controller", tabPortlets.getController().getName());
+    context.put(ALEipConstants.SECURE_ID, rundata.getUser().getTemp(
+      ALEipConstants.SECURE_ID));
+
+    ALEipUtils.setupContext(rundata, context);
+    // putData(rundata, context);
+
+    context.put("l10n", ALLocalizationUtils.createLocalization(rundata));
+    context.put("utils", new ALCommonUtils());
+
+    String layout_template = "portlets/html/ja/ajax-customize-form.vm";
+    setTemplate(rundata, context, layout_template);
   }
 
   @SuppressWarnings("unused")
