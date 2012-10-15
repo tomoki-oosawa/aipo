82ec42d074119caa280664bf5011102694f6a63a
alkoshikawa <ges.jonieet@gmail.com>
2012-10-15 05:03:46 +0000

    一旦差し戻し
    git-svn-id: http://aipo.googlecode.com/svn/trunk@4295 b7d4e767-949e-c8c6-5735-6c11b8a2f74f

--- portlets/blog/src/main/java/com/aimluck/eip/blog/BlogEntryLatestSelectData.java
+++ portlets/blog/src/main/java/com/aimluck/eip/blog/BlogEntryLatestSelectData.java
@@ -162,23 +162,12 @@ public class BlogEntryLatestSelectData extends
       int size = list.size();
       for (int i = 0; i < size; i++) {
         EipTBlogFile record = list.get(i);
-        long fileId = record.getFileId().longValue();
         BlogFileResultData file = new BlogFileResultData();
         file.initField();
-        file.setFileId(fileId);
-        file.setOwnerId(Database
-          .get(EipTBlogFile.class, fileId)
-          .getOwnerId()
-          .longValue());
-        file.setEntryId(Database
-          .get(EipTBlogFile.class, fileId)
-          .getEipTBlogEntry()
-          .getEntryId()
-          .longValue());
-        file.setEntryTitle(Database
-          .get(EipTBlogFile.class, fileId)
-          .getEipTBlogEntry()
-          .getTitle());
+        file.setFileId(record.getFileId().longValue());
+        file.setOwnerId(record.getOwnerId().longValue());
+        file.setEntryId(record.getEipTBlogEntry().getEntryId().longValue());
+        file.setEntryTitle(record.getEipTBlogEntry().getTitle());
         photoList.add(file);
       }
     }
