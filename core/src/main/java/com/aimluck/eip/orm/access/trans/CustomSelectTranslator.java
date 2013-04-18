8cfbfaff21125dd19aef78350e6b9d86552ad67d
ankankm <aimluck.nakamura@gmail.com>
2013-04-18 16:53:38 +0900

    refs #22076 findbug > com.aimluck.eip.orm.access.trans.CustomSelectTranslator.getResultColumns() は、CustomSelectTranslator.columns を返すことによって内部表現を暴露するかもしれません。

--- core/src/main/java/com/aimluck/eip/orm/access/trans/CustomSelectTranslator.java
+++ core/src/main/java/com/aimluck/eip/orm/access/trans/CustomSelectTranslator.java
@@ -26,7 +26,7 @@ import org.apache.cayenne.access.jdbc.ColumnDescriptor;
 import org.apache.cayenne.access.trans.SelectTranslator;
 
 /**
- * 
+ *
  */
 public class CustomSelectTranslator extends SelectTranslator {
 
@@ -72,7 +72,7 @@ public class CustomSelectTranslator extends SelectTranslator {
       if (columns == null || columns.length == 0) {
         return super.getResultColumns();
       } else {
-        return columns;
+        return columns.clone();
       }
     } else {
       ColumnDescriptor[] resultColumns = super.getResultColumns();
