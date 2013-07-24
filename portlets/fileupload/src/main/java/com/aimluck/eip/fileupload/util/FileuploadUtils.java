e55682af7ef941a3a648c9aa822f0fed9a716165
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2013-07-24 15:00:14 +0900

    refs #27574

--- portlets/fileupload/src/main/java/com/aimluck/eip/fileupload/util/FileuploadUtils.java
+++ portlets/fileupload/src/main/java/com/aimluck/eip/fileupload/util/FileuploadUtils.java
@@ -22,13 +22,9 @@ package com.aimluck.eip.fileupload.util;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -49,12 +45,6 @@ import com.aimluck.eip.fileupload.beans.FileuploadLiteBean;
 import com.aimluck.eip.services.storage.ALStorageService;
 import com.aimluck.eip.util.ALCommonUtils;
 import com.aimluck.eip.util.ALEipUtils;
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.exif.ExifIFD0Directory;
-import com.drew.metadata.jpeg.JpegDirectory;
 
 /**
  * ファイルアップロードのユーティリティクラスです。 <BR>
@@ -345,28 +335,7 @@ public class FileuploadUtils {
       reader.dispose();
 
       // BufferedImage orgImage = ImageIO.read(is);
-      // File fis =
-      // new File(ALStorageService.getService().toString()
-      // + "/"
-      // + FOLDER_TMP_FOR_ATTACHMENT_FILES
-      // + "/"
-      // + org_id
-      // + "/"
-      // + uid
-      // + ALStorageService.separator()
-      // + folderName
-      // + "/"
-      // + String.valueOf(fileBean.getFileId()));
-      File fis =
-        new File(ALStorageService.getFilePath(
-          FOLDER_TMP_FOR_ATTACHMENT_FILES,
-          uid + ALStorageService.separator() + folderName,
-          String.valueOf(fileBean.getFileId())));
-      orgImage =
-        transformImage(
-          orgImage,
-          getExifTransformation(readImageInformation(fis)));
-      Boolean test = fis.exists();
+
       BufferedImage shrinkImage =
         FileuploadUtils.shrinkAndTrimImage(orgImage, width, height);
       Iterator<ImageWriter> writers = ImageIO.getImageWritersBySuffix("jpg");
@@ -612,120 +581,4 @@ public class FileuploadUtils {
   public static int getMaxFileSize(String unit) {
     return ALCommonUtils.getMaxFileSize(unit);
   }
-
-  public static class ImageInformation {
-    public final int orientation;
-
-    public final int width;
-
-    public final int height;
-
-    public ImageInformation(int orientation, int width, int height) {
-      this.orientation = orientation;
-      this.width = width;
-      this.height = height;
-    }
-
-    @Override
-    public String toString() {
-      return String.format(
-        "%dx%d,%d",
-        this.width,
-        this.height,
-        this.orientation);
-    }
-  }
-
-  public static ImageInformation readImageInformation(File in) {
-    try {
-      Metadata metadata = ImageMetadataReader.readMetadata(in);
-      Directory directory = metadata.getDirectory(ExifIFD0Directory.class);
-      JpegDirectory jpegDirectory = metadata.getDirectory(JpegDirectory.class);
-
-      int orientation = 1;
-      try {
-        orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-      } catch (Exception me) {
-
-      }
-      int width = jpegDirectory.getImageWidth();
-      int height = jpegDirectory.getImageHeight();
-
-      return new ImageInformation(orientation, width, height);
-    } catch (IOException e) {
-
-    } catch (ImageProcessingException e) {
-
-    } catch (Exception e) {
-
-    }
-    return null;
-  }
-
-  public static AffineTransform getExifTransformation(ImageInformation info) {
-
-    AffineTransform t = new AffineTransform();
-    if (info == null) {
-      return t;
-    }
-
-    switch (info.orientation) {
-      case 1:
-        break;
-      case 2: // Flip X
-        t.scale(-1.0, 1.0);
-        t.translate(-info.width, 0);
-        break;
-      case 3: // PI rotation
-        t.translate(info.width, info.height);
-        t.rotate(Math.PI);
-        break;
-      case 4: // Flip Y
-        t.scale(1.0, -1.0);
-        t.translate(0, -info.height);
-        break;
-      case 5: // - PI/2 and Flip X
-        t.rotate(-Math.PI / 2);
-        t.scale(-1.0, 1.0);
-        break;
-      case 6: // -PI/2 and -width
-        t.translate(info.height, 0);
-        t.rotate(Math.PI / 2);
-        break;
-      case 7: // PI/2 and Flip
-        t.scale(-1.0, 1.0);
-        t.translate(-info.height, 0);
-        t.translate(0, info.width);
-        t.rotate(3 * Math.PI / 2);
-        break;
-      case 8: // PI / 2
-        t.translate(0, info.width);
-        t.rotate(3 * Math.PI / 2);
-        break;
-    }
-
-    return t;
-  }
-
-  public static BufferedImage transformImage(BufferedImage image,
-      AffineTransform transform) throws Exception {
-
-    AffineTransformOp op =
-      new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC);
-
-    BufferedImage destinationImage =
-      op.createCompatibleDestImage(
-        image,
-        (image.getType() == BufferedImage.TYPE_BYTE_GRAY) ? image
-          .getColorModel() : null);
-    Graphics2D g = destinationImage.createGraphics();
-    g.setBackground(Color.WHITE);
-    g
-      .clearRect(0, 0, destinationImage.getWidth(), destinationImage
-        .getHeight());
-    destinationImage = op.filter(image, destinationImage);
-    ;
-    return destinationImage;
-  }
-
 }
