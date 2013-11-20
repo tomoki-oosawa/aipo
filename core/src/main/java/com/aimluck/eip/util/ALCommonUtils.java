ab9050c9a1e0254c034f387ae06a420f77b0c1d5
ankankm <aimluck.nakamura@gmail.com>
2013-11-20 18:33:36 +0900

    refs#30200 IE8wbr対応

--- core/src/main/java/com/aimluck/eip/util/ALCommonUtils.java
+++ core/src/main/java/com/aimluck/eip/util/ALCommonUtils.java
@@ -86,7 +86,7 @@ public class ALCommonUtils {
     int j;
     for (int i = 0; i < count; i++) {
       j = i * step;
-      res.append(str.substring(j, j + step)).append("<wbr/>");
+      res.append(str.substring(j, j + step)).append("<wbr/>&shy;");
     }
     if (count * step < size) {
       res.append(str.substring(count * step));
