ebccffa68ea9e742e573129b32a51befaae610c9
k-hatano <aimluck.hatano@gmail.com>
2014-02-06 17:05:13 +0900

    refs #31006 (追加コミット)

---
+++ portlets/wiki/src/main/java/com/aimluck/eip/wiki/WikiMultiDelete.java
@@ -0,0 +1,52 @@
+/*
+ * Aipo is a groupware program developed by Aimluck,Inc.
+ * Copyright (C) 2004-2014 Aimluck,Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aimluck.eip.wiki;
+
+import java.util.List;
+
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+import com.aimluck.eip.common.ALAbstractCheckList;
+import com.aimluck.eip.common.ALDBErrorException;
+import com.aimluck.eip.common.ALPageNotFoundException;
+
+/**
+ * 
+ */
+public class WikiMultiDelete extends ALAbstractCheckList {
+
+  /**
+   * @param rundata
+   * @param context
+   * @param values
+   * @param msgList
+   * @return
+   * @throws ALPageNotFoundException
+   * @throws ALDBErrorException
+   */
+  @Override
+  protected boolean action(RunData rundata, Context context,
+      List<String> values, List<String> msgList)
+      throws ALPageNotFoundException, ALDBErrorException {
+    return false;
+  }
+
+}
