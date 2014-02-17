da04e75f4852434225a36291e77ff2407da09abc
watanabe <aimluck.watanabe@gmail.com>
2014-02-17 11:02:11 +0900

    開発 #31166 プラン強化プロジェクト > wiki機能 > 改行が省略される

--- portlets/wiki/src/main/java/com/aimluck/eip/wiki/WikiResultData.java
+++ portlets/wiki/src/main/java/com/aimluck/eip/wiki/WikiResultData.java
@@ -198,6 +198,9 @@ public class WikiResultData implements ALData {
       noteText = noteText.replaceAll("&lt;del&gt;", "<del>");
       noteText = noteText.replaceAll("&lt;/del&gt;", "</del>");
 
+      /* 改行タグ */
+      noteText = noteText.replaceAll("\n", "<br>");
+
       if (null == model) {
         model = new WikiModel("", "");
       }
