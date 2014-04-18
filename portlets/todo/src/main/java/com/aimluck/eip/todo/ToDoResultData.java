4098fd00d195506c83adf2d081922b3b59dea836
y-ozaki <aimluck.ozaki@gmail.com>
2014-04-18 10:57:23 +0900

    refs #31893

--- portlets/todo/src/main/java/com/aimluck/eip/todo/ToDoResultData.java
+++ portlets/todo/src/main/java/com/aimluck/eip/todo/ToDoResultData.java
@@ -188,6 +188,13 @@ public class ToDoResultData implements ALData {
   }
 
   /**
+   * @return
+   */
+  public String getTodoNameString() {
+    return todo_name.toString();
+  }
+
+  /**
    * @param i
    */
   public void setCategoryId(long i) {
