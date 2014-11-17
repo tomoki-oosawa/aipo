5a59c47821c1fc43ffec283d4e982487120b66a8
nbeppu <nbeppu@gmail.com>
2014-11-17 18:12:33 +0900

    refs #33744

--- war/src/main/webapp/javascript/aipo/message.js
+++ war/src/main/webapp/javascript/aipo/message.js
@@ -477,6 +477,9 @@ aipo.message.unselectRoom = function() {
         dojo.query(".messageSummary li").forEach(function(item) {
             dojo.removeClass(item, "active")
         });
+        dojo.query(".messageUserlist li").forEach(function(item) {
+            dojo.removeClass(item, "active")
+        });
         aipo.message.clearInput();
         if(messageMainBlockEmpty) {
             messageMainBlockEmpty.style.display = "";
