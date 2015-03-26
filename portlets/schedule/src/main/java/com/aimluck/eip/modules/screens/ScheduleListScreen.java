952aebae08e5de5f254b3da4f1c7760bf33ee589
nbeppu <nbeppu@gmail.com>
2015-03-26 13:28:08 +0900

    refs #34606

--- portlets/schedule/src/main/java/com/aimluck/eip/modules/screens/ScheduleListScreen.java
+++ portlets/schedule/src/main/java/com/aimluck/eip/modules/screens/ScheduleListScreen.java
@@ -44,7 +44,7 @@ import com.aimluck.eip.util.ALEipUtils;
 
 /**
  * スケジュールの一覧を処理するクラスです。 <br />
- * 
+ *
  */
 public class ScheduleListScreen extends ScheduleScreen {
 
@@ -53,7 +53,7 @@ public class ScheduleListScreen extends ScheduleScreen {
     .getLogger(ScheduleListScreen.class.getName());
 
   /**
-   * 
+   *
    * @param rundata
    * @param context
    * @throws Exception
@@ -84,6 +84,14 @@ public class ScheduleListScreen extends ScheduleScreen {
         showAll = "f";
       }
       context.put("init_s_all", showAll);
+
+      // 表示形式（月間）を取得する
+      String display_month =
+        portlet.getPortletConfig().getInitParameter("p195-rows");
+      if (display_month == null || "".equals(display_month)) {
+        display_month = "detail";
+      }
+      context.put("display_month", display_month);
 
       String has_acl_other = ScheduleUtils.hasAuthOther(rundata);
       context.put("hasAcl", has_acl_other);
