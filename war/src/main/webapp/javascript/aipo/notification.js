b46e3c4d5ecda055c647cef7ff07fc4e8a421cd1
nbeppu <nbeppu@gmail.com>
2015-05-21 02:58:01 +0900

    refs #33672

--- war/src/main/webapp/javascript/aipo/notification.js
+++ war/src/main/webapp/javascript/aipo/notification.js
@@ -19,30 +19,121 @@
 dojo.provide("aipo.notification");
 
 aipo.notification.setUp = function() {
-    var notificationRequestField = dojo.byId("notificationRequestField");
-    var notificationConfirmField = dojo.byId("notificationConfirmField");
-    var notificationSuccessField = dojo.byId("notificationSuccessField");
-    if(window.Notification && window.Notification.permission!="granted") {
-	    notificationRequestField.style.display="";
+    var notificationRequestField = dojo.query(".notificationRequestField");
+    var notificationConfirmField = dojo.query(".notificationConfirmField");
+    var notificationSuccessField = dojo.query(".notificationSuccessField");
+    if(aipo.notification.isDefault()) {
+    	dojo.forEach(notificationRequestField,function(item){item.style.display="";});
     }
 }
 
-aipo.notification.requestPermission = function() {
-    var notificationRequestField = dojo.byId("notificationRequestField");
-    var notificationConfirmField = dojo.byId("notificationConfirmField");
-    var notificationSuccessField = dojo.byId("notificationSuccessField");
-    window.Notification.requestPermission(function() {
-    	if(window.Notification.permission == "granted") {
-            notificationRequestField.style.display = "none";
-            notificationConfirmField.style.display = "none";
-            notificationSuccessField.style.display = "";
-        } else if(window.Notification.permission == "default") {
-            notificationSuccessField.style.display = "none";
-            notificationConfirmField.style.display = "none";
-            notificationRequestField.style.display = "";
+aipo.notification.hasFeature = function() {
+	return window.Notification || window.webkitNotifications;
+}
+
+aipo.notification.isGranted = function() {
+	return (window.Notification && window.Notification.permission == "granted") || (window.webkitNotifications && window.webkitNotifications.checkPermission() == 0);
+}
+
+aipo.notification.isDefault = function() {
+	return (window.Notification && window.Notification.permission == "default") || (window.webkitNotifications && window.webkitNotifications.checkPermission() == 1);
+}
+
+aipo.notification.requestPermission = function(callback) {
+	if(window.Notification) {
+	    window.Notification.requestPermission(callback);
+	} else if(window.webkitNotifications) {
+	    window.webkitNotifications.requestPermission(callback);
+	}
+}
+
+aipo.notification.enableDesktopNotification = function(enabled) {
+    var handleJSONResponse = function(obj) {
+        if (obj.rc == 200) {
+            var data = obj.data;
+            if(data){
+                aipo.activityDesktopNotifyEnable = data.enable;
+            }
         }
-    });
-    notificationRequestField.style.display = "none";
-    notificationSuccessField.style.display = "none";
-    notificationConfirmField.style.display = "";
+    };
+
+    var request = {
+    };
+
+    var makeRequestParams = {
+       "CONTENT_TYPE" : "JSON",
+       "METHOD" : "POST",
+       "POST_DATA" : gadgets.json.stringify(request)
+    };
+
+    var url = "?template=ActivityNotifyEnableJSONScreen&enable=" + enabled;
+
+    gadgets.io.makeNonProxiedRequest(url,
+            handleJSONResponse,
+            makeRequestParams,
+            "application/javascript");
+}
+
+aipo.notification.requestDesktopNotification = function(portletId) {
+    var notificationRequestField = dojo.query(".notificationRequestField");
+    var notificationConfirmField = dojo.query(".notificationConfirmField");
+    var notificationSuccessField = dojo.query(".notificationSuccessField");
+
+    var callback = function() {
+    	if(aipo.notification.isGranted()) {
+        	dojo.forEach(notificationRequestField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationConfirmField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationSuccessField,function(item){item.style.display="";});
+            aipo.notification.enableDesktopNotification('T');
+            if(portletId) {
+                dojo.byId("desktopNotification" + portletId + "_T").checked = true;
+            }
+        } else if(aipo.notification.isDefault()) {
+        	dojo.forEach(notificationSuccessField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationConfirmField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationRequestField,function(item){item.style.display="";});
+        } else {
+        	dojo.forEach(notificationSuccessField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationConfirmField,function(item){item.style.display="none";});
+        	dojo.forEach(notificationRequestField,function(item){item.style.display="none";});
+        	dojo.forEach(dojo.query(".notificationBlockField"),function(item){item.style.display="";});
+        }
+    };
+    aipo.notification.requestPermission(callback);
+	dojo.forEach(notificationRequestField,function(item){item.style.display="none";});
+	dojo.forEach(notificationSuccessField,function(item){item.style.display="none";});
+	dojo.forEach(notificationConfirmField,function(item){item.style.display="";});
+}
+
+
+
+aipo.notification.closeRequest = function() {
+    var notificationField = dojo.byId("notificationField");
+    if(notificationField) {
+    	notificationField.style.display="none";
+    }
+}
+
+aipo.notification.openDialog = function() {
+	aipo.menu.hideDropdownAll();
+	aipo.notification.closeRequest();
+	if (window.Notification) {
+		aipo.common.showDialog("?template=ActivityNotificationFormScreen&s=1&p=" + window.Notification.permission);
+    } else if (window.webkitNotifications) {
+    	aipo.common.showDialog("?template=ActivityNotificationFormScreen&s=1&p=" + window.webkitNotifications.checkPermission());
+    } else  {
+    	aipo.common.showDialog("?template=ActivityNotificationFormScreen&s=0");
+    }
+}
+
+aipo.notification.onReceiveMessage = function(msg){
+    if(!msg) {
+        var arrDialog = dijit.byId("modalDialog");
+        if(arrDialog){
+            arrDialog.hide();
+        }
+    }
+    if (dojo.byId('messageDiv')) {
+        dojo.byId('messageDiv').innerHTML = msg;
+    }
 }
