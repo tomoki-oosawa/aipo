0b95a09c6465cce1e2e232b3fe855d25f168b4d4
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2015-10-21 14:21:16 +0900

    Revert "Merge pull request #938 from aipocom/features/message-push-delete-oneself"
    This reverts commit 34d637251953da9aac9f13eebb8a239abc88c93e, reversing
    changes made to ef10b44afb8fbb83c7f1a9aab6464bd45fa85ff1.

--- portlets/message/src/main/java/com/aimluck/eip/message/MessageFormData.java
+++ portlets/message/src/main/java/com/aimluck/eip/message/MessageFormData.java
@@ -344,8 +344,8 @@ public class MessageFormData extends ALAbstractFormData {
             if (member.getUserId().intValue() != (int) login_user
               .getUserId()
               .getValue()) {
+              recipients.add(member.getLoginName());
             }
-            recipients.add(member.getLoginName());
           }
         }
       }
