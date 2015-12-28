715e13f71a9bf5e36e5cdc8fb0e8cc2628dd2b52
kymr16 <kym.m1116@gmail.com>
2015-12-28 15:25:14 +0900

    refs #31252

---
+++ portlets/account/src/main/webapp/javascript/aipo/account_position/form.js
@@ -0,0 +1,48 @@
+/*
+ * Aipo is a groupware program developed by TOWN, Inc.
+ * Copyright (C) 2004-2015 TOWN, Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+dojo.provide("aipo.account_position");
+
+aipo.account_position.onReceiveMessage = function(msg){
+    if(!msg) {
+        var arrDialog = dijit.byId("modalDialog");
+        if(arrDialog){
+            arrDialog.hide();
+        }
+        aipo.portletReload('account_company');
+    }
+    if (dojo.byId('messageDiv')) {
+        dojo.byId('messageDiv').innerHTML = msg;
+    }
+}
+
+aipo.account_position.sortsubmit=function(form){
+	  var s_o = form.member_so.options;
+	  var tmp = "";
+	  for(i = 0 ; i < s_o.length; i++ ) {
+	    s_o[i].selected = false;
+	  }
+	  if(s_o.length > 0) {
+		  tmp=s_o[0].value;
+		    for(i = 1 ; i < s_o.length; i++ ) {
+		      tmp =tmp+','+ s_o[i].value ;
+		    }
+	  }
+	  form.positions.value =tmp;
+	}
