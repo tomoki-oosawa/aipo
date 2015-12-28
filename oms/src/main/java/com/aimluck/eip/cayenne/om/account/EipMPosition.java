ec076fe89b443a7dc162f8865b29d83b915f3a66
kymr16 <kym.m1116@gmail.com>
2015-12-28 17:48:51 +0900

    refs #31252
    -　Aipo8.1 > 役職 > 順番設定機能
    - cayenneによるマッピング周り修正

--- oms/src/main/java/com/aimluck/eip/cayenne/om/account/EipMPosition.java
+++ oms/src/main/java/com/aimluck/eip/cayenne/om/account/EipMPosition.java
@@ -24,54 +24,6 @@ import com.aimluck.eip.cayenne.om.account.auto._EipMPosition;
 
 public class EipMPosition extends _EipMPosition {
 
-  public static final String CREATE_DATE_PROPERTY = "createDate";
-
-  public static final String POSITION_NAME_PROPERTY = "positionName";
-
-  public static final String SORT_PROPERTY = "sort";
-
-  public static final String UPDATE_DATE_PROPERTY = "updateDate";
-
-  public static final String POSITION_ID_PK_COLUMN = "POSITION_ID";
-
-  @Override
-  public void setCreateDate(java.util.Date createDate) {
-    writeProperty("createDate", createDate);
-  }
-
-  @Override
-  public java.util.Date getCreateDate() {
-    return (java.util.Date) readProperty("createDate");
-  }
-
-  @Override
-  public void setPositionName(String positionName) {
-    writeProperty("positionName", positionName);
-  }
-
-  @Override
-  public String getPositionName() {
-    return (String) readProperty("positionName");
-  }
-
-  public void setSort(Integer sort) {
-    writeProperty("sort", sort);
-  }
-
-  public Integer getSort() {
-    return (Integer) readProperty("sort");
-  }
-
-  @Override
-  public void setUpdateDate(java.util.Date updateDate) {
-    writeProperty("updateDate", updateDate);
-  }
-
-  @Override
-  public java.util.Date getUpdateDate() {
-    return (java.util.Date) readProperty("updateDate");
-  }
-
   public Integer getPositionId() {
     if (getObjectId() != null && !getObjectId().isTemporary()) {
       Object obj = getObjectId().getIdSnapshot().get(POSITION_ID_PK_COLUMN);
