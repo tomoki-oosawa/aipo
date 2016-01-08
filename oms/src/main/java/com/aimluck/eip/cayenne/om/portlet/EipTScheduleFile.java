bf6750cd32c8f765b433b8da59ce2d00460899b3
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2016-01-08 16:35:26 +0900

    refs #34506
    スケジュール > ファイル添付機能
    
    CayenneModelerによるORM部分対応

---
+++ oms/src/main/java/com/aimluck/eip/cayenne/om/portlet/EipTScheduleFile.java
@@ -0,0 +1,28 @@
+package com.aimluck.eip.cayenne.om.portlet;
+
+import org.apache.cayenne.ObjectId;
+
+import com.aimluck.eip.cayenne.om.portlet.auto._EipTScheduleFile;
+
+public class EipTScheduleFile extends _EipTScheduleFile {
+
+  public Integer getFileId() {
+    if (getObjectId() != null && !getObjectId().isTemporary()) {
+      Object obj = getObjectId().getIdSnapshot().get(FILE_ID_PK_COLUMN);
+      if (obj instanceof Long) {
+        Long value = (Long) obj;
+        return Integer.valueOf(value.intValue());
+      } else {
+        return (Integer) obj;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  public void setFileId(String id) {
+    setObjectId(new ObjectId("EipTScheduleFile", FILE_ID_PK_COLUMN, Integer
+      .valueOf(id)));
+  }
+
+}
