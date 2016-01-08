5018d66634b2b014f6332a832cbf5f46efbc7a78
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2016-01-08 14:29:18 +0900

    refs #31252

--- portlets/account/src/main/webapp/javascript/aipo/account_company/form.js
+++ portlets/account/src/main/webapp/javascript/aipo/account_company/form.js
@@ -31,7 +31,7 @@ aipo.account_company.onLoadPostDialog = function(portlet_id){
         for(i = 0 ; i < s_o.length; i ++ ) {
             memberlist.addOptionSync(s_o[i].value,s_o[i].text,true);
         }
-    
+
         dojo.byId("post_name").focus();
     }
 }
@@ -70,4 +70,17 @@ aipo.account_company.onReceiveMessage = function(msg){
     }
 }
 
-
+aipo.account_company.sortsubmit=function(form){
+	  var s_o = form.member_so.options;
+	  var tmp = "";
+	  for(i = 0 ; i < s_o.length; i++ ) {
+	    s_o[i].selected = false;
+	  }
+	  if(s_o.length > 0) {
+		  tmp=s_o[0].value;
+		    for(i = 1 ; i < s_o.length; i++ ) {
+		      tmp =tmp+','+ s_o[i].value ;
+		    }
+	  }
+	  form.positions.value =tmp;
+	}
