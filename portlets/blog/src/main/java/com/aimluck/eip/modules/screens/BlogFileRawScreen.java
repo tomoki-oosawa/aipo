568dc21e6f589e57d1a8e2fbc942abdf7c2cc6da
nbeppu <nbeppu@192.168.3.249>
2016-02-04 14:13:34 +0900

    refs #36715

--- portlets/blog/src/main/java/com/aimluck/eip/modules/screens/BlogFileRawScreen.java
+++ portlets/blog/src/main/java/com/aimluck/eip/modules/screens/BlogFileRawScreen.java
@@ -6,7 +6,7 @@ z * Aipo is a groupware program developed by TOWN, Inc.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later versionzz.
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
