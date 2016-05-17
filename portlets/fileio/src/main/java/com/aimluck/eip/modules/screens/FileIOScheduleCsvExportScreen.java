8306abb733878b7d32f0c2b1a2300ce8c970ed53
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2016-05-17 18:34:46 +0900

    refs #37440

---
+++ portlets/fileio/src/main/java/com/aimluck/eip/modules/screens/FileIOScheduleCsvExportScreen.java
@@ -0,0 +1,267 @@
+/*
+ * Aipo is a groupware program developed by TOWN, Inc.
+ * Copyright (C) 2004-2015 TOWN, Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aimluck.eip.modules.screens;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Locale;
+
+import org.apache.jetspeed.services.logging.JetspeedLogFactoryService;
+import org.apache.jetspeed.services.logging.JetspeedLogger;
+import org.apache.turbine.services.TurbineServices;
+import org.apache.turbine.util.RunData;
+
+import com.aimluck.eip.cayenne.om.portlet.VEipTScheduleList;
+import com.aimluck.eip.common.ALPermissionException;
+import com.aimluck.eip.facility.beans.FacilityLiteBean;
+import com.aimluck.eip.facility.util.FacilityUtils;
+import com.aimluck.eip.schedule.ScheduleExportListContainer;
+import com.aimluck.eip.schedule.ScheduleExportResultData;
+import com.aimluck.eip.schedule.util.ScheduleUtils;
+import com.aimluck.eip.services.accessctl.ALAccessControlConstants;
+import com.aimluck.eip.services.accessctl.ALAccessControlFactoryService;
+import com.aimluck.eip.services.accessctl.ALAccessControlHandler;
+import com.aimluck.eip.services.orgutils.ALOrgUtilsService;
+import com.aimluck.eip.util.ALEipUtils;
+import com.aimluck.eip.util.ALLocalizationUtils;
+
+/**
+ *
+ *
+ */
+public class FileIOScheduleCsvExportScreen extends ALCSVScreen {
+  /** logger */
+  private static final JetspeedLogger logger = JetspeedLogFactoryService
+    .getLogger(FileIOScheduleCsvExportScreen.class.getName());
+
+  private int userid;
+
+  /** 閲覧権限の有無 */
+  private boolean hasAclviewOther;
+
+  private ScheduleExportListContainer con;
+
+  private List<Integer> scheduleIdList;
+
+  /**
+   *
+   * @param rundata
+   * @return
+   */
+  @Override
+  protected String getContentType(RunData rundata) {
+    return "application/octet-stream";
+  }
+
+  /**
+   *
+   * @param rundata
+   * @return
+   * @throws Exception
+   */
+  @Override
+  protected String getCSVString(RunData rundata) throws Exception {
+    if (ALEipUtils.isAdmin(rundata)) {
+      userid = ALEipUtils.getUserId(rundata);
+      ALAccessControlFactoryService aclservice =
+        (ALAccessControlFactoryService) ((TurbineServices) TurbineServices
+          .getInstance())
+          .getService(ALAccessControlFactoryService.SERVICE_NAME);
+      ALAccessControlHandler aclhandler = aclservice.getAccessControlHandler();
+      hasAclviewOther =
+        aclhandler.hasAuthority(
+          userid,
+          ALAccessControlConstants.POERTLET_FEATURE_SCHEDULE_OTHER,
+          ALAccessControlConstants.VALUE_ACL_LIST);
+      scheduleIdList = new ArrayList<Integer>();
+
+      Date viewStart =
+        DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.JAPAN).parse(
+          rundata.getParameters().get("start_day"));
+      Date viewEnd =
+        DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.JAPAN).parse(
+          rundata.getParameters().get("end_day"));
+
+      // 有効なユーザーを全て取得する
+      List<Integer> userIds = ALEipUtils.getUserIds("LoginUser");
+      int userid = ALEipUtils.getUserId(rundata);
+      // 有効な設備を全て取得する
+      List<FacilityLiteBean> facilityLiteBeans =
+        FacilityUtils.getFacilityLiteBeans(rundata);
+      ArrayList<Integer> facilityList = new ArrayList<Integer>();
+      for (FacilityLiteBean facility : facilityLiteBeans) {
+        facilityList.add(Integer.valueOf(facility.getFacilityId()));
+      }
+
+      List<VEipTScheduleList> scheduleList =
+        ScheduleUtils.getScheduleList(
+          userid,
+          viewStart,
+          viewEnd,
+          userIds,
+          facilityList,
+          true);
+      List<VEipTScheduleList> resultList =
+        ScheduleUtils.sortByDummySchedule(scheduleList);
+
+      con = new ScheduleExportListContainer();
+      con.initField();
+      Calendar cal5 = Calendar.getInstance();
+      cal5.setTime(viewStart);
+      con.setViewStartDate(cal5);
+      Calendar cal6 = Calendar.getInstance();
+      cal6.setTime(viewEnd);
+      con.setViewEndDate(cal6);
+
+      String LINE_SEPARATOR = System.getProperty("line.separator");
+      try {
+        StringBuffer sb = new StringBuffer();
+        sb.append("開始日,開始時刻,終了日,終了時刻,場所,タイトル,内容,参加者,設備");
+
+        for (ListIterator<VEipTScheduleList> iterator =
+          resultList.listIterator(resultList.size()); iterator.hasPrevious();) {
+          getResultData(iterator.previous());
+        }
+        for (ScheduleExportResultData record : con.getScheduleList()) {
+          sb.append(LINE_SEPARATOR);
+          sb.append("\"");
+          sb.append(record.getViewDate());
+          sb.append("\",\"");
+          sb.append(record.getStartDate());
+          sb.append("\",\"");
+          sb.append(record.getEndDateExport());
+          sb.append("\",\"");
+          sb.append(record.getEndDate());
+          sb.append("\",\"");
+          sb.append(record.getPlace());
+          sb.append("\",\"");
+          sb.append(record.getName());
+          sb.append("\",\"");
+          sb.append(record.getNoteExport());
+          sb.append("\"");
+        }
+
+        return sb.toString();
+      } catch (Exception e) {
+        logger.error("FileIOScheduleCsvFileScreen.getCSVString", e);
+        return null;
+      }
+    } else {
+      throw new ALPermissionException();
+    }
+  }
+
+  /**
+   * @param previous
+   * @return
+   */
+  private ScheduleExportResultData getResultData(VEipTScheduleList record) {
+    ScheduleExportResultData rd = new ScheduleExportResultData();
+    rd.initField();
+    try {
+      // スケジュールが棄却されている場合は表示しない
+      if ("R".equals(record.getStatus())) {
+        return rd;
+      }
+
+      boolean is_member = record.isMember();
+
+      // Dummy スケジュールではない
+      // 完全に隠す
+      // 自ユーザー以外
+      // 共有メンバーではない
+      // オーナーではない
+      if ((!"D".equals(record.getStatus()))
+        && "P".equals(record.getPublicFlag())
+        && (userid != record.getUserId().intValue())
+        && (userid != record.getOwnerId().intValue())
+        && !is_member) {
+        return null;
+      }
+      if ("C".equals(record.getPublicFlag())
+        && ("F".equals(record.getType()) || ("U".equals(record.getType()) && userid != record
+          .getUserId()
+          .intValue()))
+        && (userid != record.getOwnerId().intValue())
+        && !is_member) {
+        rd.setName(ALLocalizationUtils.getl10n("SCHEDULE_CLOSE_PUBLIC_WORD"));
+        // 仮スケジュールかどうか
+        rd.setTmpreserve(false);
+      } else {
+        rd.setName(record.getName());
+        // 仮スケジュールかどうか
+        rd.setTmpreserve("T".equals(record.getStatus()));
+      }
+
+      if (!hasAclviewOther && !is_member) {// 閲覧権限がなく、グループでもない
+        return rd;
+      }
+
+      // ID
+      rd.setScheduleId(record.getScheduleId().intValue());
+      // 親スケジュール ID
+      rd.setParentId(record.getParentId().intValue());
+      // 開始日時
+      rd.setStartDate(record.getStartDate());
+      // 終了日時
+      rd.setEndDate(record.getEndDate());
+      // 公開するかどうか
+      rd.setPublic("O".equals(record.getPublicFlag()));
+      // 非表示にするかどうか
+      rd.setHidden("P".equals(record.getPublicFlag()));
+      // ダミーか
+      rd.setDummy("D".equals(record.getStatus()));
+      // ログインユーザかどうか
+      rd.setLoginuser(record.getUserId().intValue() == userid);
+      // オーナーかどうか
+      rd.setOwner(record.getOwnerId().intValue() == userid);
+      // 共有メンバーかどうか
+      rd.setMember(is_member);
+      // 繰り返しパターン
+      rd.setPattern(record.getRepeatPattern());
+      rd.setCreateUser(ALEipUtils.getALEipUser(record.getCreateUserId()));
+      rd.setNote(record.getNote());
+      rd.setPlace(record.getPlace());
+      rd.setDescription(record.getNote());
+
+      if (!rd.getPattern().equals("N") && !rd.getPattern().equals("S")) {
+        rd.setRepeat(true);
+      }
+      if (!scheduleIdList.contains(rd.getScheduleId().getValueWithInt())) {
+        scheduleIdList.add(rd.getScheduleId().getValueWithInt());
+        con.addResultData(rd);
+      }
+      // ToDo member,facilitiesリスト作成
+
+    } catch (Exception e) {
+      logger.error("schedule", e);
+      return null;
+    }
+    return rd;
+  }
+
+  @Override
+  protected String getFileName() {
+    return ALOrgUtilsService.getAlias() + "_schedules.csv";
+  }
+}
---
+++ portlets/schedule/src/main/java/com/aimluck/eip/schedule/ScheduleExportResultData.java
@@ -0,0 +1,183 @@
+/*
+ * Aipo is a groupware program developed by TOWN, Inc.
+ * Copyright (C) 2004-2015 TOWN, Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aimluck.eip.schedule;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aimluck.commons.field.ALStringField;
+import com.aimluck.eip.common.ALEipHolidaysManager;
+import com.aimluck.eip.common.ALEipUser;
+import com.aimluck.eip.facilities.FacilityResultData;
+import com.aimluck.eip.util.ALLocalizationUtils;
+
+/**
+ * スケジュールのCSVエクスポートデータを管理するクラスです。
+ *
+ * ScheduleDetailResultDataのデータにScheduleSearchResultDataのデータおよび参加者、設備のリストを追加
+ *
+ */
+public class ScheduleExportResultData extends ScheduleDetailResultData {
+
+  /** <code>members</code> 共有メンバー */
+  private final List<ALEipUser> members = new ArrayList<ALEipUser>();
+
+  /** <code>facilities</code> 共有設備 */
+  private final List<FacilityResultData> facilities =
+    new ArrayList<FacilityResultData>();
+
+  @Override
+  public void initField() {
+    super.initField();
+  }
+
+  /**
+   * @return members
+   */
+  public List<ALEipUser> getMembers() {
+    return members;
+  }
+
+  public void addMember(ALEipUser user) {
+    members.add(user);
+  }
+
+  /**
+   * @return facilities
+   */
+  public List<FacilityResultData> getFacilities() {
+    return facilities;
+  }
+
+  public void addFacility(FacilityResultData facility) {
+    facilities.add(facility);
+  }
+
+  public String getDateFull() {
+    SimpleDateFormat sdf =
+      new SimpleDateFormat(ALLocalizationUtils
+        .getl10n("SCHEDULE_SIMPLE_DATE_FORMAT"));
+    StringBuilder b = new StringBuilder();
+    if ("S".equals(getPattern())) {
+      b.append(sdf.format(getStartDate().getValue()));
+    } else {
+      b.append(sdf.format(getStartDate().getValue())).append(" ").append(
+        getDate2());
+    }
+
+    return b.toString();
+  }
+
+  public String getDateFullOnlyDate() {
+    SimpleDateFormat sdf =
+      new SimpleDateFormat(ALLocalizationUtils
+        .getl10n("SCHEDULE_SIMPLE_DATE_FORMAT"));
+    StringBuilder b = new StringBuilder();
+    b.append(sdf.format(getStartDate().getValue()));
+    return b.toString();
+  }
+
+  public String getDateShort() {
+    SimpleDateFormat sdf =
+      new SimpleDateFormat(ALLocalizationUtils
+        .getl10n("SCHEDULE_SIMPLE_DATE_FORMAT_SHORT"));
+    StringBuilder b = new StringBuilder();
+    b.append(sdf.format(getStartDate().getValue()));
+    return b.toString();
+  }
+
+  public String getPatternTime() {
+    StringBuilder b = new StringBuilder();
+    if ("S".equals(getPattern())) {
+      if (isTerm()) {
+        b.append(ALLocalizationUtils.getl10n("SCHEDULE_TERM"));
+      } else {
+        b.append(ALLocalizationUtils.getl10n("SCHEDULE_ALL_DAY"));
+      }
+    } else {
+      b.append(getDate());
+    }
+    return b.toString();
+  }
+
+  /**
+   * 時刻まではチェックしない。
+   *
+   * @return 年・月・日が等しいならtrue
+   */
+  public boolean isStartDayEqualsEndDay() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    String startDate = sdf.format(getStartDate().getValue());
+    String endDate = sdf.format(getEndDate().getValue());
+    return startDate.equals(endDate);
+  }
+
+  public boolean isStartDayEqualsToday() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    String startDate = sdf.format(getStartDate().getValue());
+    String toDay = sdf.format(new java.util.Date());
+    return startDate.equals(toDay);
+  }
+
+  public boolean isStartDateHoliday() {
+    return ALEipHolidaysManager.getInstance().isHoliday(
+      getStartDate().getValue()) != null;
+  }
+
+  public String getDateDetail() {
+    SimpleDateFormat sdf =
+      new SimpleDateFormat(ALLocalizationUtils
+        .getl10n("SCHEDULE_SIMPLE_DATE_FORMAT"));
+    StringBuilder b = new StringBuilder();
+    if ("S".equals(getPattern())) {
+      b.append(sdf.format(getStartDate().getValue()));
+      if (!isStartDayEqualsEndDay()) {
+        b.append("&nbsp;");
+        b.append("-");
+        b.append("&nbsp;");
+        b.append(sdf.format(getEndDate().getValue()));
+      }
+    } else {
+      b.append(sdf.format(getStartDate().getValue())).append(" ").append(
+        getDate());
+    }
+    return b.toString();
+  }
+
+  public String getViewDate() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    sdf.format(getStartDate().getValue());
+    return sdf.format(getStartDate().getValue());
+  }
+
+  public String getEndDateExport() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    sdf.format(getEndDate().getValue());
+    return sdf.format(getEndDate().getValue());
+  }
+
+  public ALStringField getNoteExport() {
+    ALStringField field = new ALStringField();
+
+    String str = getNoteStr().replaceAll("\r", "").replaceAll("\n", " ");
+    field.setValue(str);
+    return field;
+  }
+}
