67f494c14e6f360b457b5f1536d7e4206579f0eb
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2016-07-08 14:49:23 +0900

    refs #37789

--- portlets/message/src/main/java/com/aimluck/eip/message/util/MessageUtils.java
+++ portlets/message/src/main/java/com/aimluck/eip/message/util/MessageUtils.java
@@ -60,6 +60,7 @@ import com.aimluck.eip.orm.query.Operations;
 import com.aimluck.eip.orm.query.ResultList;
 import com.aimluck.eip.orm.query.SQLTemplate;
 import com.aimluck.eip.orm.query.SelectQuery;
+import com.aimluck.eip.services.orgutils.ALOrgUtilsService;
 import com.aimluck.eip.services.push.ALPushService;
 import com.aimluck.eip.services.storage.ALStorageService;
 import com.aimluck.eip.util.ALEipUtils;
@@ -514,10 +515,10 @@ public class MessageUtils {
     if (isSearch) {
       if (isMySQL) {
         body
-          .append(" and ((t2.room_type='G' and t2.name like #bind($keyword)) or (t2.room_type='O' and CONCAT( (case when t4.login_name='admin' then 'Aipo.com' else t4.last_name end) ,t4.first_name) like #bind($keyword))) ");
+          .append(" and ((t2.room_type='G' and t2.name like #bind($keyword)) or (t2.room_type='O' and CONCAT( (case when t4.login_name='admin' then #bind($alias) else t4.last_name end) ,t4.first_name) like #bind($keyword))) ");
       } else {
         body
-          .append(" and ((t2.room_type='G' and t2.name like #bind($keyword)) or (t2.room_type='O' and ( (case when t4.login_name='admin' then 'Aipo.com' else t4.last_name end) || t4.first_name) like #bind($keyword))) ");
+          .append(" and ((t2.room_type='G' and t2.name like #bind($keyword)) or (t2.room_type='O' and ( (case when t4.login_name='admin' then #bind($alias) else t4.last_name end) || t4.first_name) like #bind($keyword))) ");
       }
     }
 
@@ -531,6 +532,7 @@ public class MessageUtils {
         .param("user_id", Integer.valueOf(userId));
     if (isSearch) {
       countQuery.param("keyword", "%" + keyword + "%");
+      countQuery.param("alias", ALOrgUtilsService.getAlias());
     }
 
     int countValue = 0;
@@ -566,6 +568,7 @@ public class MessageUtils {
         Integer.valueOf(userId));
     if (isSearch) {
       query.param("keyword", "%" + keyword + "%");
+      query.param("alias", ALOrgUtilsService.getAlias());
     }
 
     List<DataRow> fetchList = query.fetchListAsDataRow();
