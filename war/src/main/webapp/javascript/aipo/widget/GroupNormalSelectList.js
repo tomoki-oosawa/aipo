c8538d2660d0af6218d614e95c2dd6bc3004ff20
Kazutoshi Shinoda <kazutoshi.shinoda0516@gmail.com>
2016-09-13 18:04:15 +0900

    refs #38444 Aipo.com > Webメール > iPhoneで参加者や設備を選択しようとすると1つ目の要素が勝手に追加される

--- war/src/main/webapp/javascript/aipo/widget/GroupNormalSelectList.js
+++ war/src/main/webapp/javascript/aipo/widget/GroupNormalSelectList.js
@@ -44,6 +44,11 @@ dojo.declare("aipo.widget.GroupNormalSelectList", [dijit._Widget, dijit._Templat
     postCreate: function(){
         this.id = this.widgetId;
 
+        // iphone9系において先頭のoptionタグが勝手に選択されてしまう問題対策
+        if (navigator.userAgent.indexOf("iPhone OS 9") > -1){
+        	this.addDummyOptGroup();
+        }
+
         params = {
           url: this.memberFromUrl,
           key: this.memberFromOptionKey,
@@ -53,6 +58,28 @@ dojo.declare("aipo.widget.GroupNormalSelectList", [dijit._Widget, dijit._Templat
     },
     addOption:function(select, value, text, is_selected) {
       aimluck.io.addOption(select, value, text, is_selected);
+    },
+    addDummyOptGroup:function() {
+    	var addDummy = function(select) {
+    		var flag = true;
+    		if(select.children.length > 0) {
+    			var head = select.children[0];
+    			flag = !(head.nodeName == "OPTGROUP" && head.children.length == 0);
+    		}
+    		if(flag) {
+    			var dummy = document.createElement("optgroup");
+    			dummy.disabled = 'true';
+    			dummy.style.display = 'none';
+    			dummy.classList.add('dummy');
+    			if (document.all) {
+    				select.add(dummy, 0);
+    			} else {
+    				select.insertBefore(dummy, select.options[0]);
+    			}
+    		}
+    	};
+    	addDummy(dojo.byId(this.memberToId));
+    	addDummy(dojo.byId(this.memberFromId));
     },
     addOptionSync:function(value, text, is_selected) {
       var select = dojo.byId(this.memberToId);
