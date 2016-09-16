9d1a65b86e8d52e849be9b90ddd59023c210b860
KinoshitaYosuke <k1410159@edu.cc.uec.ac.jp>
2016-09-16 12:49:09 +0900

    intern 5/10day 午前のみの部分

--- portlets/schedule/src/main/java/com/aimluck/eip/schedule/util/ScheduleUtils.java
+++ portlets/schedule/src/main/java/com/aimluck/eip/schedule/util/ScheduleUtils.java
@@ -971,6 +971,25 @@ public class ScheduleUtils {
     boolean result = false;
     Calendar cal = Calendar.getInstance();
     cal.setTime(date.getValue());
+
+    /*
+     * 休日情報を取得 if(isUserHoliday(1~7)) if(isUserHoliday(x)==TRUE && ptn(x)==1){
+     * switch(ptn(ptn.length())){ case ''A": 次の日にずらす; break; case "B": 前の日にずらす;
+     * break; case "D": 繰り返さない; break; case "N": ずらさない; break; default: break; }
+     * }
+     * 
+     * 毎週●曜日の場合 A:ptn(x+1)=1,ptn(x)=0 ->(x+1)が曜日の枠を超えたら->x=sunday B:ptn(x-1)=1,
+     * ptn(x)=0 ->(x-1)が曜日の枠を超えたら->x=saturday D:ptn(x)=0
+     * 
+     * 毎週第●曜日の場合
+     * 
+     * 
+     * 毎月の場合
+     * 
+     * 
+     * 毎年の場合
+     */
+
     // 毎日
     if (ptn.charAt(0) == 'D') {
       result = true;
@@ -6288,4 +6307,8 @@ public class ScheduleUtils {
     return null;
 
   }
+
+  /*
+   * 休日の設定を行う所
+   */
 }
