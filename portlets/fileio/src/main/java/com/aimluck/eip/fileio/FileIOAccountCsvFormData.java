bdb8579229f5c3016fb40d4ead1b0ea963e78107
t-funaki <funatomo01mail@gmail.com>
2016-11-21 15:02:31 +0900

    refs #39334 インポート時にパスワードの変更日時がNULLにならないように変更

--- portlets/fileio/src/main/java/com/aimluck/eip/fileio/FileIOAccountCsvFormData.java
+++ portlets/fileio/src/main/java/com/aimluck/eip/fileio/FileIOAccountCsvFormData.java
@@ -567,6 +567,7 @@ public class FileIOAccountCsvFormData extends ALAbstractFormData {
           .getValue()));
         // JetspeedSecurity.forcePassword(user, password.getValue());
         user.setPassword(password.getValue());
+        user.setPasswordChanged(new Date());
         isNewUser = true;
         user.setCreated(now);
         user.setLastLogin(now);
