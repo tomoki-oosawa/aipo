9e430c21b7888b0c07e6473d089ceb2f52a600d7
elecho1 <rikiauczatu@gmail.com>
2017-03-14 09:58:17 +0900

    new added file

---
+++ portlets/exttimecard/src/main/java/com/aimluck/eip/modules/screens/ExtTimecardListScreenPrint.java
@@ -0,0 +1,70 @@
+/*
+ * Aipo is a groupware program developed by TOWN, Inc.
+ * Copyright (C) 2004-2015 TOWN, Inc.
+ * http://www.aipo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aimluck.eip.modules.screens;
+
+import org.apache.jetspeed.services.logging.JetspeedLogFactoryService;
+import org.apache.jetspeed.services.logging.JetspeedLogger;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+import com.aimluck.eip.exttimecard.ExtTimecardSelectData;
+import com.aimluck.eip.exttimecard.util.ExtTimecardUtils;
+import com.aimluck.eip.util.ALEipUtils;
+
+/**
+ * ToDoの印刷画面を処理するクラスです。 <br />
+ * ->ExtTimecard
+ */
+
+public class ExtTimecardListScreenPrint extends ALVelocityScreen {
+
+  /** logger */
+  private static final JetspeedLogger logger = JetspeedLogFactoryService
+    .getLogger(ExtTimecardListScreenPrint.class.getName());
+
+  /**
+   *
+   * @param rundata
+   * @param context
+   * @throws Exception
+   */
+  @Override
+  protected void doOutput(RunData rundata, Context context) throws Exception {
+    try {
+      ExtTimecardSelectData detailData = new ExtTimecardSelectData();
+      detailData.initField();
+      detailData.doViewDetail(this, rundata, context);
+
+      String layout_template = "portlets/html/ajax-exttimecard-list-print.vm";
+      setTemplate(rundata, context, layout_template);
+    } catch (Exception ex) {
+      logger.error("[ExtTimecardListScreenPrint] Exception.", ex);
+      ALEipUtils.redirectDBError(rundata);
+    }
+  }
+
+  /**
+   * @return
+   */
+  @Override
+  protected String getPortletName() {
+    return ExtTimecardUtils.EXTTIMECARD_PORTLET_NAME;
+  }
+
+}
