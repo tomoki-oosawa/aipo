7d066d14d3d07cc33ec45424b679fabecfe391b2
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2017-04-11 14:15:56 +0900

    refs #39550

--- sql/postgres/initdb.d/org001.sql
+++ sql/postgres/initdb.d/org001.sql
@@ -17,9 +17,6 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-CREATE DATABASE org001 ENCODING 'UTF8';
-\c org001;
-
 -----------------------------------------------------------------------------
 -- AIPO_LICENSE
 -----------------------------------------------------------------------------
