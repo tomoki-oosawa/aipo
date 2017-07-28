63bbb9271ad96a81f851167cb2b5472ef2c9c302
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2017-07-28 17:18:51 +0900

    refs #34778
    既存ユーザーの他ユーザーのスケジュールに関する操作のアクセス権限はいろいろと変更してある可能性が高いため、acl_typeはそのままとします。

--- sql/mysql/8110to8200.sql
+++ sql/mysql/8110to8200.sql
@@ -71,7 +71,7 @@ UPDATE `eip_t_acl_portlet_feature` SET `acl_type` = 63 WHERE `feature_name` = 's
 UPDATE `eip_t_acl_portlet_feature` SET `acl_type` = 63 WHERE `feature_name` = 'schedule_other' AND `feature_alias_name` = 'スケジュール（他ユーザーの予定）操作';
 UPDATE `eip_t_acl_portlet_feature` SET `acl_type` = 44 WHERE `feature_name` = 'schedule_facility' AND `feature_alias_name` = 'スケジュール（設備の予約）操作';
 UPDATE `eip_t_acl_role` SET `acl_type` = 63, `note` = '＊追加、編集、削除、外部入力は一覧表示の権限を持っていないと使用できません' WHERE feature_id IN (SELECT feature_id FROM eip_t_acl_portlet_feature WHERE feature_name = 'schedule_self') AND `role_name` = 'スケジュール（自分の予定）管理者';
-UPDATE `eip_t_acl_role` SET `acl_type` = 35, `role_name` = 'スケジュール（他ユーザーの予定）管理者', `note` = '＊追加、編集、削除、外部入力は一覧表示の権限を持っていないと使用できません' WHERE feature_id IN (SELECT feature_id FROM eip_t_acl_portlet_feature WHERE feature_name = 'schedule_other') AND `role_name` = 'スケジュール（他ユーザーの予定）';
+UPDATE `eip_t_acl_role` SET `role_name` = 'スケジュール（他ユーザーの予定）管理者', `note` = '＊追加、編集、削除、外部入力は一覧表示の権限を持っていないと使用できません' WHERE feature_id IN (SELECT feature_id FROM eip_t_acl_portlet_feature WHERE feature_name = 'schedule_other') AND `role_name` = 'スケジュール（他ユーザーの予定）';
 UPDATE `eip_t_acl_role` SET `acl_type` = 44 WHERE feature_id IN (SELECT feature_id FROM eip_t_acl_portlet_feature WHERE feature_name = 'schedule_facility') AND `role_name` = 'スケジュール（設備の予約）管理者';
 -- 20170425
 
